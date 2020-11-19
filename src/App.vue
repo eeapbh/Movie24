79ac4521b599142687d48d62e723048757650bca
jes5918 <wjsdmltntn@gmail.com>
2020-11-19 11:14:31 +0900

    start front

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Community</router-link> |
+      <router-link to="/community">Community</router-link> |
     </div>
     <router-view/>
   </div>
