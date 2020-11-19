baa7b57fb0d37a8ee3457a18c4b82bd179e38174
eeapbh <eeapbh@gmail.com>
2020-11-19 17:25:27 +0900

    movies

---
+++ src/components/MoviesCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <img :src="getImage" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title"><b>{{ getTitle }}</b></h5>
+      <p class="card-text">{{ getOverview }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: Object
+  },
+  computed: {
+    getImage: function() {
+      return this.movie.poster_path
+    },
+    getTitle: function() {
+      return this.movie.title
+    },
+    getOverview: function() {
+      return this.movie.overview
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
