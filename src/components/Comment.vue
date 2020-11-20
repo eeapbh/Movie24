e64cca5095a797308442ffeb376d79fecd01798e
dongjay00 <cjswodlehdgn@gmail.com>
2020-11-20 19:49:58 +0900

    comment-create

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>댓글</div>
+  <div>
+    <p>{{ getComment }} - {{ getname }}</p>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data() {
+    return {
+      getname: '',
+    }
+  },
   props: {
     comment: Object
   },
+  computed: {
+    getComment() {
+      return this.comment.content
+    }
+  },
+  created() {
+    const userid = this.comment.user
+    axios({
+      url: `http://127.0.0.1:8000/api/v1/accounts/${userid}`,
+      method: 'GET',
+    }).then((res)=>{
+      // console.log(res)
+      this.getname = res.data.username
+    }).catch((err)=>{
+      console.error(err)
+    })
+  }
 }
 </script>
 
