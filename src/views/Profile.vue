d8c5c0a8a076ddc51be48a1dcf191c6bcdc77de2
hyunwoo <hw2621@daum.net>
2020-11-20 19:41:37 +0900

    done profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,20 +1,61 @@
 <template>
-  <div>프로필</div>
+  <div>
+    <h1>{{username}}의 프로필</h1>
+    <br><br>
+    <h2>작성한 글</h2>
+    <span v-for= "(article,idx) in articles" :key = "idx">
+      <li>{{article.title}}</li>
+    </span>
+    <br><br>
+    <h2>작성한 댓글</h2>
+    <span v-for= "(comment,idx) in comments" :key = "idx">
+      <li>{{comment.content}}</li>
+    </span>
+  </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 import jwt_decode from 'jwt-decode'
+
 export default {
-  // data() {
-  //   userInfo: []
-  // },
+  data: function () {
+    return {
+      username: '',
+      articles: [],
+      comments: [],
+    }
+  },
   created() {
-    // axios({
-    //   url: 'http://127.0.0.1:8000/api/v1/accounts/'
-    // })
+    
     const token = localStorage.getItem('jwt')
-    console.log(jwt_decode(token))
+    const decoded = jwt_decode(token)
+    console.log(decoded)
+    this.username = decoded['username']
+    const userId = decoded['user_id']
+    // console.log(userId)
+    // 유저 articles
+    axios({
+      url: 'http://127.0.0.1:8000/api/v1/articles/' + String(userId) + '/profile_articles/',
+      method: 'GET'
+    }).then((res)=>{
+      console.log(res)
+      this.articles = res.data
+    }).catch((err)=>{
+      console.log(err)
+    })
+    
+    // 유저 comments
+    axios({
+      url: 'http://127.0.0.1:8000/api/v1/articles/' + String(userId) + '/profile_comments/',
+      method: 'GET'
+    }).then((res)=>{
+      console.log(res)
+      this.comments = res.data
+    }).catch((err)=>{
+      console.log(err)
+    })
+  
   }
 }
 </script>
