6507e9d20c9685d0b37eddbd05a1feb74bf553ad
eeapbh <eeapbh@gmail.com>
2020-11-23 01:14:58 +0900

    moviecomment

--- src/components/MovieComment.vue
+++ src/components/MovieComment.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-    <p>{{ getComment }} - {{ getName }}</p>
+    <p>평점:{{getRating}} - {{ getComment }} - {{ getName }}</p>
     <button v-if="getName == currentName" @click="deleteComment">Delete</button>
   </span>
 </template>
@@ -13,6 +13,7 @@ export default {
     return {
       getName: '',
       currentName: '',
+      
     }
   },
   props: {
@@ -22,13 +23,15 @@ export default {
   computed: {
     getComment() {
       return this.comment.content
-    }
+    },
+    getRating() {
+      return this.comment.rating
+    },
   },
   methods: {
     deleteComment(event) {
       event.preventDefault()
-      const review_pk = this.movie_pk
-      console.log(review_pk)
+      const review_pk = this.comment.id
       axios({
         url: `http://127.0.0.1:8000/movies/${review_pk}/review_delete/` ,
         method: 'DELETE'
@@ -46,7 +49,6 @@ export default {
       url: `http://127.0.0.1:8000/api/v1/accounts/${userid}/`,
       method: 'GET',
     }).then((res)=>{
-      // console.log(res)
       this.getName = res.data.username
     }).catch((err)=>{
       console.error(err)
