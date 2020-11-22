5f65f9091d6a8aa8c61d4f735f28b5ac2be3c48a
dongjay00 <cjswodlehdgn@gmail.com>
2020-11-22 18:14:48 +0900

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <span v-if="login">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/movies">Movies</router-link> |
-        <router-link to="/community">Community</router-link> |
-        <router-link to="/create-article">CreateArticle</router-link> |
-        <router-link to="/profile">Profile</router-link> |
-        <router-link @click.native="logout" to="#">Logout</router-link> |
-      </span>
-      <span v-else>
-        <router-link to="/signup">Signup</router-link> |
-        <router-link to="/login">Login</router-link>
-      </span>
-    </div>
+    <b-navbar class="sticky-top" toggleable="lg" type="dark" variant="dark">
+      <router-link class="navbar-brand" to="/">Home</router-link>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav v-if="login">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/movies">Movies</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/community">Community</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/profile">Profile</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" @click.native="logout" to="#">Logout</router-link>
+            </li>
+          </ul>
+        </b-navbar-nav>
+        <b-navbar-nav v-else>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/signup">Signup</router-link> 
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+          </ul>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <p></p>
     <router-view @login="login = true" />
   </div>
 </template>
