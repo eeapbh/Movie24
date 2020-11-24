aea59b9768395b378143b79e836b41e882035fdc
hyunwoo <hw2621@daum.net>
2020-11-24 22:09:40 +0900

    recommend base

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,30 @@
 <template>
   <div>
     <h1>Home</h1> 
+    
+    <Sliders
+      :movies="recommendmovies"
+      :title="recommendTitle"
+    />
+    <Sliders
+      :movies="horrormovies"
+      :title="horrorTitle"
+    />
+    <Sliders
+      :movies="nowmovies"
+      :title="nowTitle"
+    />
+    <Sliders
+      :movies="SFmovies"
+      :title="SFTitle"
+    />
+    <Sliders
+      :movies="Adventuremovies"
+      :title="AdventureTitle"
+    />
+    
 
-    <h5 class='text-left ml-3'>이번달 영화순위</h5>
-    <swiper :options = "swiperOptions" ref = "best">
-      <RecommendMovieCard
-        v-for="(movie, idx) in recommendmovies"
-        :key="idx"
-        :movie="movie"
-        class="col-lg-2 col-md-4 col-sm-6 col-xs-12"
-        
-      />
-    <div class="swiper-pagination" slot="pagination"></div>
-      <div 
-        class="swiper-button-prev swiper-button-white" 
-        slot="button-prev" 
-        @click = "Bestprev"
-      >
-      
-
-      </div>
-      <div 
-      class="swiper-button-next swiper-button-white" 
-      slot="button-next" 
-      @click = "Bestnext"
-      >
-      </div>
-    </swiper>
-    <br>
-    <h5 class='text-left ml-3'>{{ username }}님의 취향저격 베스트 컨텐츠</h5>
+    <h4 class='text-left ml-3'>{{ username }}님의 취향저격 베스트 컨텐츠</h4>
     <swiper :options = "swiperOptions" ref = "rec">
         <ForUserMovie
           v-for="(forusermovie, idx) in forusermovies"
@@ -58,20 +54,42 @@
 </template>
 
 <script>
-import RecommendMovieCard from '../components/RecommendMovieCard.vue'
+// import RecommendMovieCard from '../components/RecommendMovieCard.vue'
 import ForUserMovie from '../components/ForUserMovie.vue'
+import Sliders from '../components/Sliders.vue'
 import axios from 'axios'
 import jwt_decode from 'jwt-decode'
+
 export default {
   components: {
-    RecommendMovieCard,
+    // RecommendMovieCard,
     ForUserMovie,
+    // 슬라이더
+    Sliders,
   },
   data() {
     return {
-      recommendmovies:[],
+      //얘는 특별관리
       forusermovies:[],
+      //배열들
+      recommendmovies:[],
+      horrormovies:[],
+      SFmovies:[],
+      Adventuremovies:[],
+      similarmovies1:[],
+      similarmovies2:[],
+      nowmovies:[],
+      //제목들
+      recommendTitle:'평론가 호평을 받은 영화',
+      horrorTitle:'호러 영화',
+      SFTitle:'SF 영화',
+      AdventureTitle:'어드벤처 영화',
+      nowTitle:'현재 상영중인 영화',
+
+
+      //유저이름
       username:'',
+      //스와이퍼옵션
       swiperOptions: {
         slidesPerView: 'auto',
         
@@ -99,23 +117,17 @@ export default {
       }
       this.$refs.rec.$swiper.slideNext()
     },
-    Bestprev(){
-      for (let i = 0; i < 5; i++){
-        this.$refs.best.$swiper.slidePrev()
-      }
-    },
-    Bestnext(){
-      for (let i = 0; i < 5; i++){
-        this.$refs.best.$swiper.slideNext()
-      }
-    },
   },
   
   created() {
+    // token에서 유저 상세 정보 뺴옴
     const token = localStorage.getItem('jwt')
     console.log(jwt_decode(token))
     const user = jwt_decode(token).user_id
     this.username=jwt_decode(token).username
+    //끝
+
+    //평점 높은 영화 : 차라리 toprated로 바꾸자.
     axios({
       url:'http://127.0.0.1:8000/movies/bestFive/',
       method:'GET',
@@ -129,42 +141,179 @@ export default {
     }).catch((err)=>{
       console.error(err)
     }),
-    axios({
-      url:`http://127.0.0.1:8000/api/v1/accounts/${user}/recommend/`,
-      method: 'GET',
-    }).then((res)=> {
-      console.log(res.data)
-      const movieId = res.data.fav_movie
+    //끝
+
+    // 유저가 평점 잘 준 영화와 비슷한 영화, DB에 저장까지
       axios({
-        url:`https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=a03503b78be406a84d592df5327b4dbd&language=kr-KO&page=1`,
-        method: 'GET'
-      }).then((res)=>{
+        url:`http://127.0.0.1:8000/api/v1/accounts/${user}/recommend/`,
+        method: 'GET',
+      }).then((res)=> {
+        console.log(res.data)
+        const movieId = res.data.fav_movie
+        axios({
+          url:`https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=a03503b78be406a84d592df5327b4dbd&language=kr-KO&page=1`,
+          method: 'GET'
+        }).then((res)=>{
         const tmp = []
         console.log('resdata',res.data)
         res.data.results.forEach(function(element){
           tmp.push(element)
         })
-        this.forusermovies = tmp
-        axios({
-          url: 'http://127.0.0.1:8000/movies/forUserMovieSave/ ',
-          method: 'POST',
-          data: {
-            forusermovies:this.forusermovies,
-          },
-          headers: {
-            Authorization: `JWT ${localStorage.getItem('jwt')}`
-          },
-        }).then((res)=>{
-          console.log(res.data)
-        }).catch((err)=>{
-          console.error(err)
-        })
+      this.forusermovies = tmp
+      axios({
+        url: 'http://127.0.0.1:8000/movies/forUserMovieSave/ ',
+        method: 'POST',
+        data: {
+          forusermovies:this.forusermovies,
+        },
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('jwt')}`
+        },
+      }).then(()=>{
+        console.log('취향저격','DB에 저장')
+      }).catch((err)=>{
+        console.error(err)
+      })
         // console.log(this.forusermovies)
       })
-    })
+      })
+    //끝
     
-  },
+    //호러
+    axios({
+      url:'https://api.themoviedb.org/3/discover/movie?api_key=a03503b78be406a84d592df5327b4dbd&language=kr-KO&page=1',
+      method: 'GET',
+      data: {
+          with_genres: 27,
+          page: 1,
+          language:'ko-KR'
+      }
+    }).then((res)=>{
+      const tmp = []
+      console.log('호러',res.data)
+      res.data.results.forEach(function(element){
+        tmp.push(element)
+    })
+    this.horrormovies = tmp
+    })
+    axios({
+        url: 'http://127.0.0.1:8000/movies/forUserMovieSave/ ',
+        method: 'POST',
+        data: {
+          forusermovies:this.horrormovies,
+        },
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('jwt')}`
+        },
+      }).then(()=>{
+        console.log('호러 DB저장')
+      }).catch((err)=>{
+        console.error(err)
+      })
+    //끝
+    
+    //SF
+    axios({
+      url:'https://api.themoviedb.org/3/discover/movie?api_key=a03503b78be406a84d592df5327b4dbd&language=kr-KO&page=1',
+      method: 'GET',
+      data: {
+          with_genres: 878,
+          page: 1,
+          language:'ko-KR'
+      }
+    }).then(
+      (res)=>{
+      const tmp = []
+      console.log('SF',res.data)
+      res.data.results.forEach(
+        function(element){
+        tmp.push(element)
+    })
+    this.SFmovies = tmp
+    })
+    axios({
+        url: 'http://127.0.0.1:8000/movies/forUserMovieSave/ ',
+        method: 'POST',
+        data: {
+          forusermovies:this.SFmovies,
+        },
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('jwt')}`
+        },
+      }).then(()=>{
+        console.log('SF DB저장')
+      }).catch((err)=>{
+        console.error(err)
+      })
+    //끝
+
+    //Adventure
+    axios({
+      url:'https://api.themoviedb.org/3/discover/movie?api_key=a03503b78be406a84d592df5327b4dbd&language=kr-KO&page=1',
+      method: 'GET',
+      data: {
+          with_genres: 12,
+          page: 1,
+          language:'ko-KR'
+      }
+    }).then((res)=>{
+      const tmp = []
+      console.log('어드벤처',res.data)
+      res.data.results.forEach(function(element){
+        tmp.push(element)
+    })
+    this.Adventuremovies = tmp
+    })
+    axios({
+        url: 'http://127.0.0.1:8000/movies/forUserMovieSave/ ',
+        method: 'POST',
+        data: {
+          forusermovies:this.Adventuremovies,
+        },
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('jwt')}`
+        },
+      }).then(()=>{
+        console.log('어드벤처 DB저장')
+      }).catch((err)=>{
+        console.error(err)
+      })
+    //끝
+    
+
+    //상영중영화
+    axios({
+      url:'https://api.themoviedb.org/3/movie/now_playing?api_key=a03503b78be406a84d592df5327b4dbd&language=kr-KO&page=1',
+      method: 'GET',
+      data: {
+          page: 1,
+          language:'ko-KR'
+      }
+    }).then((res)=>{
+      const tmp = []
+      console.log('상영중',res.data)
+      res.data.results.forEach(function(element){
+        tmp.push(element)
+    })
+    this.nowmovies = tmp
+    })
+    axios({
+        url: 'http://127.0.0.1:8000/movies/forUserMovieSave/ ',
+        method: 'POST',
+        data: {
+          forusermovies:this.nowmovies,
+        },
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('jwt')}`
+        },
+      }).then(()=>{
+        console.log('상영중 DB저장')
+      }).catch((err)=>{
+        console.error(err)
+      })
+    //끝
   
+  }  
 }
 </script>
 
