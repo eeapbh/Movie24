470f28870c3f097ebc06b910ef3ade768da1647b
hyunwoo <hw2621@daum.net>
2020-11-25 18:32:01 +0900

    linechange

--- src/components/ForUserMovie.vue
+++ src/components/ForUserMovie.vue
@@ -4,7 +4,7 @@
       <div class="card-img"><img :src="getImage" class="card-img-top" alt=""></div>
       
       <div class="card-body">
-        <h5 class="card-title"><b>{{ getTitle }}</b></h5>
+        <h5 class="card-title" v-for="(item,idx) in getTitle" :key="idx"><b>{{ item }}</b></h5>
       </div>
     </div>
     <b-modal 
@@ -41,7 +41,29 @@ export default {
       return 'http://image.tmdb.org/t/p/w185'+this.forusermovie.poster_path
     },
     getTitle: function() {
-      return this.forusermovie.title
+      const t = this.forusermovie.title
+      // const len = length(t)
+      const temp = t.split(' ')
+      // let res= ''
+      // temp.foreach(function(element){
+        // if (length(res) + length(element) > 12){
+        // res =  res+ element
+        // } else {
+        //   res = res + 'x' + element
+        // }
+      // })
+      let res = []
+      let tp = ''
+      for(let i = 0; i < temp.length; i++){
+        if(tp.length + temp[i].length < 12 ){ 
+          tp += ' ' + temp[i]
+        }else{
+          res.push(tp)
+          tp = temp[i]
+        }
+      }
+      res.push(tp)
+      return res
     },
     getOverview: function() {
       return this.forusermovie.overview
